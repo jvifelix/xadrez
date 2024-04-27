345fadd7c00ca3ddb6c453e8b87fe33cdb8f7b2c
João Victor <victoralmeida.va2@gmail.com>
2024-04-27 00:32:10 -0300

    deixando o tabuleiro bonito

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -30,5 +30,10 @@ public class ChessPosition {
 		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
 	}
 	
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+	
 	
 }
