84b8abef89731d13a03d1cabcb7d4fecffab3182
João Victor <victoralmeida.va2@gmail.com>
2024-04-28 11:41:05 -0300

    Adicionando o movimento das peças

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,14 +1,32 @@
 package app;
 
+import static java.lang.System.out;
+
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.UI;
 
 public class  Program {
 	public static void main(String args[]) {
 		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println();
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(scanner);
+			out.println();
+			out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(scanner);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+			
+		}
 	}
 }
--- src/chess/UI.java
+++ src/chess/UI.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UI {
 
 	public static final String ANSI_RESET = "\u001B[0m";
@@ -21,6 +24,19 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static ChessPosition readChessPosition(Scanner scanner) {
+		try {
+			String s = scanner.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch(RuntimeException e){
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+		}
+		
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 
 		for (int i = 0; i < pieces.length; i++) {
